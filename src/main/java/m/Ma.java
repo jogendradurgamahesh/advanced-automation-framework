ea0cb6bcc39330e40696510a66ff5a865e7b5156
jogendradurgamahesh <jogendramahesh123@gmail.com>
2026-03-25 11:41:34 +0530

    j

--- src/main/java/m/Ma.java
+++ src/main/java/m/Ma.java
@@ -3,7 +3,11 @@ package m;
 public class Ma {
 
 	public static void main(String[] args) {
+<<<<<<< HEAD
 		System.out.println("Mahiii");
+=======
+		System.out.println("Mahiiiii");
+>>>>>>> f9ea734 (q)
 
 	}
 
