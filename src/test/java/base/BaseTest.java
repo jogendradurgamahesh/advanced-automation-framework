d664b990350fbc46cfdedfbe759501aa59ccae74
Uggina Jogendra Durga Mahesh <151815160+jogendradurgamahesh@users.noreply.github.com>
2026-03-25 11:04:39 +0530

    Update BaseTest.java

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -2,7 +2,7 @@ package base;
 import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
-
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
